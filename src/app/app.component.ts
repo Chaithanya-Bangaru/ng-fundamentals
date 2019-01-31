ffb993dac20022ed2e830be070aa6322fc0b1a8f
Chaithanya <chaithanya.bangaru@github.com>
2019-01-30 21:16:06 -0600

    added assets defined in angularo.json, --save  ngf-bootstrap module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'events-app-root',
+  template: `<h3>Hello Ivaan Vijay Bangaru. This is programming Angular!</h3>
+              <img src="/assets/images/basic-shield.png" />
+              
+            `
 })
-export class AppComponent {
+export class EventsAppComponent {
   title = 'ng-fundamentals';
 }
